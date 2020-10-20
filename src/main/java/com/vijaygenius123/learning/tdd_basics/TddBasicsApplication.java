88f5b50bfdb942ae36c490ab6c4d8594c4184feb
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-20 22:05:57 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/learning/tdd_basics/TddBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.tdd_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddBasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/learning/tdd_basics/TddBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.tdd_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
